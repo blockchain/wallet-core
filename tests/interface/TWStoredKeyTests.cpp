530600338a4a5268fa796a18fac7ab3bcf17e787
Bartosz Tarnowski <bartosz@commerceblock.com>
2020-04-14 13:18:55 +0100

    Fix for tests.

--- tests/interface/TWStoredKeyTests.cpp
+++ tests/interface/TWStoredKeyTests.cpp
@@ -38,19 +38,16 @@ struct TWStoredKey *_Nonnull createDefaultStoredKey() {
 }
 
 TEST(TWStoredKey, loadPBKDF2Key) {
-    const auto pbkdf2keyPath = string(TESTS_ROOT) + "/Keystore/Data/pbkdf2.json";
-    //const auto filename = WRAPS(TWStringCreateWithUTF8Bytes(pbkdf2keyPath.c_str()));
-    const auto key = TWStoredKeyLoad(&pbkdf2keyPath);
-    ASSERT_NE(key, nullptr);
+    const auto filename = WRAPS(TWStringCreateWithUTF8Bytes((TESTS_ROOT + "/Keystore/Data/pbkdf2.json").c_str()));
+    const auto key = TWStoredKeyLoad(filename.get());
     const auto keyId = WRAPS(TWStoredKeyIdentifier(key));
     EXPECT_EQ(string(TWStringUTF8Bytes(keyId.get())), "3198bc9c-6672-5ab3-d995-4942343ae5b6");
     TWStoredKeyDelete(key);
 }
 
 TEST(TWStoredKey, loadNonexistent) {
-    const string nonexistentPath = string(TESTS_ROOT) + "_NO_/_SUCH_/_FILE_";
-    //const auto filenameInvalid = WRAPS(TWStringCreateWithUTF8Bytes(nonexistentPath.c_str()));
-    EXPECT_EQ(TWStoredKeyLoad(&nonexistentPath), nullptr);
+    const auto filenameInvalid = WRAPS(TWStringCreateWithUTF8Bytes((TESTS_ROOT + "_NO_/_SUCH_/_FILE_").c_str()));
+    EXPECT_EQ(TWStoredKeyLoad(filenameInvalid.get()), nullptr);
 }
 
 TEST(TWStoredKey, createWallet) {
@@ -174,9 +171,9 @@ TEST(TWStoredKey, importJsonInvalid) {
 }
 
 TEST(TWStoredKey, fixAddresses) {
-    const string password = "password";
+    const auto password = "password";
     const auto key = createAStoredKey(TWCoinTypeBitcoin, password);
-    EXPECT_TRUE(TWStoredKeyFixAddresses(key, &password));
+    EXPECT_TRUE(TWStoredKeyFixAddresses(key, WRAPS(TWStringCreateWithUTF8Bytes(password)).get()));
     TWStoredKeyDelete(key);
 }
 
@@ -195,10 +192,9 @@ TEST(TWStoredKey, importInvalidKey) {
 }
 
 TEST(TWStoredKey, removeAccountForCoin) {
-    const string password = "password";
-    const string name = "Test KeyStore";
-    auto key = TWStoredKeyCreate(&name, &password);
-    auto wallet = TWStoredKeyWallet(key, &password);
+    auto password = "password";
+    auto key = TWStoredKeyCreate("Test KeyStore", password);
+    auto wallet = TWStoredKeyWallet(key, password);
     
     ASSERT_NE(TWStoredKeyAccountForCoin(key, TWCoinTypeEthereum, wallet), nullptr);
     ASSERT_NE(TWStoredKeyAccountForCoin(key, TWCoinTypeBitcoin, wallet), nullptr);
